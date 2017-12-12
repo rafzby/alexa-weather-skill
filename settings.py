c8dccb12bbd965a318e55cd03648a538675d5fb7
rafzby <rafal.zbytniewski@gmail.com>
2017-12-12 21:11:32 +0100

    Save open weather map url in settings

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path, verbose=True)
 
 OPEN_WEATHER_MAP = {
+    'BASE_URL': 'http://api.openweathermap.org/data/2.5/weather',
     'API_KEY': os.environ.get('API_KEY'),
     'CITY': os.environ.get('CITY'),
 }
